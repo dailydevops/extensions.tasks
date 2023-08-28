763391533d18dbf68e2525e29e6e1c9c6115cbae
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-08-28 16:55:05 +0200

    chore: Created solution

--- new-project.ps1
+++ new-project.ps1
@@ -45,6 +45,6 @@ New-Project `
   -DisableTests $DisableTests `
   -DisableUnitTests $DisableUnitTests `
   -DisableIntegrationTests $DisableIntegrationTests `
-  -SolutionFile "###SOLUTION###" `
+  -SolutionFile "./Extensions.Tasks.sln" `
   -OutputDirectory (Get-Location) `
   -EnableProjectGrouping $EnableProjectGrouping
