f2fe348a1eea63bdc6e9d7256453a91aeda53fae
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-08-29 10:23:01 +0200

    fix: Code smell: False Positive `csharpsquid:S1135`
    see https://sonarcloud.io/organizations/dailydevops/rules?open=csharpsquid%3AS1135&rule_key=csharpsquid%3AS1135

--- src/NetEvolve.Extensions.Tasks/TaskOfTExtensions.cs
+++ src/NetEvolve.Extensions.Tasks/TaskOfTExtensions.cs
@@ -13,17 +13,17 @@ public static class TaskOfTExtensions
     /// <summary>
     /// Returns a <see cref="bool" /> that will complete when any of the supplied <see cref="ValueTask"/> objects have completed.
     /// </summary>
-    /// <param name="todo"><see cref="ValueTask"/> to be accomplished.</param>
+    /// <param name="task"><see cref="ValueTask"/> to be accomplished.</param>
     /// <param name="timeoutInMilliseconds">The number of milliseconds to wait before there is a negative result.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    /// <returns>true, if <paramref name="todo"/> was successful.</returns>
+    /// <returns>true, if <paramref name="task"/> was successful.</returns>
     public static async ValueTask<bool> WithTimeoutAsync(
-        this ValueTask todo,
+        this ValueTask task,
         int timeoutInMilliseconds,
         CancellationToken cancellationToken = default
     )
     {
-        var todoTask = todo.AsTask();
+        var todoTask = task.AsTask();
         var winner = await Task.WhenAny(
                 todoTask,
                 Task.Delay(timeoutInMilliseconds, cancellationToken)
@@ -35,63 +35,63 @@ public static class TaskOfTExtensions
     /// <summary>
     /// Returns a <see cref="bool" /> that will complete when any of the supplied <see cref="Task"/> objects have completed.
     /// </summary>
-    /// <param name="todo"><see cref="Task"/> to be accomplished.</param>
+    /// <param name="task"><see cref="Task"/> to be accomplished.</param>
     /// <param name="timeoutInMilliseconds">The number of milliseconds to wait before there is a negative result.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    /// <returns>true, if <paramref name="todo"/> was successful.</returns>
+    /// <returns>true, if <paramref name="task"/> was successful.</returns>
     public static async Task<bool> WithTimeoutAsync(
-        this Task todo,
+        this Task task,
         int timeoutInMilliseconds,
         CancellationToken cancellationToken = default
     )
     {
-        Argument.ThrowIfNull(todo);
+        Argument.ThrowIfNull(task);
 
-        var winner = await Task.WhenAny(todo, Task.Delay(timeoutInMilliseconds, cancellationToken))
+        var winner = await Task.WhenAny(task, Task.Delay(timeoutInMilliseconds, cancellationToken))
             .ConfigureAwait(false);
-        return winner == todo;
+        return winner == task;
     }
 
     /// <summary>
     /// Returns a <see cref="bool" /> that will complete when any of the supplied <see cref="Task{TResult}"/> objects have completed.
     /// </summary>
-    /// <param name="todo"><see cref="Task{TResult}"/> to be accomplished.</param>
+    /// <param name="task"><see cref="Task{TResult}"/> to be accomplished.</param>
     /// <param name="timeoutInMilliseconds">The number of milliseconds to wait before there is a negative result.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    /// <returns>true, if <paramref name="todo"/> was successful.</returns>
+    /// <returns>true, if <paramref name="task"/> was successful.</returns>
     [SuppressMessage(
         "Usage",
         "VSTHRD003:Avoid awaiting foreign Tasks",
         Justification = "As designed."
     )]
     public static async Task<(bool isValid, T result)> WithTimeoutAsync<T>(
-        this Task<T> todo,
+        this Task<T> task,
         int timeoutInMilliseconds,
         CancellationToken cancellationToken = default
     )
     {
-        Argument.ThrowIfNull(todo);
+        Argument.ThrowIfNull(task);
 
-        var winner = await Task.WhenAny(todo, Task.Delay(timeoutInMilliseconds, cancellationToken))
+        var winner = await Task.WhenAny(task, Task.Delay(timeoutInMilliseconds, cancellationToken))
             .ConfigureAwait(false);
-        var result = await todo.ConfigureAwait(false);
-        return (winner == todo, result);
+        var result = await task.ConfigureAwait(false);
+        return (winner == task, result);
     }
 
     /// <summary>
     /// Returns a <see cref="bool" /> that will complete when any of the supplied <see cref="ValueTask{TResult}"/> objects have completed.
     /// </summary>
-    /// <param name="todo"><see cref="ValueTask{TResult}"/> to be accomplished.</param>
+    /// <param name="task"><see cref="ValueTask{TResult}"/> to be accomplished.</param>
     /// <param name="timeoutInMilliseconds">The number of milliseconds to wait before there is a negative result.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    /// <returns>true, if <paramref name="todo"/> was successful.</returns>
+    /// <returns>true, if <paramref name="task"/> was successful.</returns>
     public static async ValueTask<(bool isValid, T result)> WithTimeoutAsync<T>(
-        this ValueTask<T> todo,
+        this ValueTask<T> task,
         int timeoutInMilliseconds,
         CancellationToken cancellationToken = default
     )
     {
-        var todoTask = todo.AsTask();
+        var todoTask = task.AsTask();
         var winner = await Task.WhenAny(
                 todoTask,
                 Task.Delay(timeoutInMilliseconds, cancellationToken)
