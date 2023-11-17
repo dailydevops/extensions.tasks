4fc9e118598a21e27cbf65823b40cee7e0f89d10
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-11-17 21:56:49 +0100

    chore: Reordered usings

--- src/NetEvolve.Extensions.Tasks/TaskExtensions/WithTimeoutAsync(ValueTask,int).cs
+++ src/NetEvolve.Extensions.Tasks/TaskExtensions/WithTimeoutAsync(ValueTask,int).cs
@@ -1,8 +1,8 @@
 ﻿namespace NetEvolve.Extensions.Tasks;
 
-using NetEvolve.Arguments;
 using System.Threading;
 using System.Threading.Tasks;
+using NetEvolve.Arguments;
 
 /// <summary>
 /// Extension methods for <see cref="Task"/>, <see cref="Task{TResult}" />, <see cref="ValueTask"/> and <see cref="ValueTask{TResult}"/>.
